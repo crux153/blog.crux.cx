95ea6f25f9f6d572720a263dbced5e36ce7af9d8
Crux <crux153@gmail.com>
2019-05-07 14:40:02 +0900

    Enable Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-38275835-2`,
       },
     },
     `gatsby-plugin-feed`,
